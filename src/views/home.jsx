7d57ff0dff83398e75846fe193b867f7e273e6af
ningyunhan <ningyunhan19940829@gmail.com>
2018-06-01 23:25:18 -0700

    react router done

---
+++ src/views/home.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Home extends Component {
+
+    render () {
+        return (
+            <div>
+                This is a home component!!!
+            </div>
+        );
+    }
+}
+
+export default Home;

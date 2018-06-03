f7c01e6f5a3836a0d87c360eccb818f6d2d96c67
ningyunhan <ningyunhan19940829@gmail.com>
2018-06-02 22:41:06 -0700

    nested route done

--- src/views/Message.jsx
+++ src/views/Message.jsx
@@ -2,11 +2,35 @@ import React, {Component} from 'react'
 
 class Message extends Component {
 
+
+    state = {
+        messages: [],
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            const messages = [
+                {id: 1, title: "message001"},
+                {id: 3, title: "message003"},
+                {id: 5, title: "message005"}
+            ];
+
+            this.setState({messages});
+        }, 1000)
+    }
+
+
     render() {
         return (
-            <div>
-
-            </div>
+            <ul>
+                {
+                    this.state.messages.map((item, index) => (
+                        <li>
+                            <a href="???">{item.title}</a>
+                        </li>
+                    ))
+                }
+            </ul>
         );
     }
 }

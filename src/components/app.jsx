94636e7d1d5d8b0b5c86736d8a98886ab5372fa5
ningyunhan <ningyunhan19940829@gmail.com>
2018-06-03 23:54:06 -0700

    react without redux done

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,24 +1,70 @@
-import React, {Component} from 'react'
-
-import Search from './search'
-import Main from './main'
+import React, {Component} from 'react';
 
 class App extends Component {
 
     state = {
-        searchName: '',
+        count: 0,
     }
 
-    setSearchName = (searchName) => {
-        this.setState({searchName});
-    }
+
+
+    increment = () => {
+        const num = this.select.value * 1;
+
+        const count = this.state.count + num;
+
+        this.setState({count})
+    };
+
+    decrement = () => {
+        const num = this.select.value * 1;
+
+        const count = this.state.count - num;
+
+        this.setState({count})
+    };
+
+
+
+    incrementIfOdd = () => {
+        const num = this.select.value * 1;
+
+        let count = this.state.count;
+        if(count % 2 === 1) {
+            count += num;
+            this.setState({count})
+        }
+    };
+
+
+    incrementIfAsync = () => {
+        const num = this.select.value * 1;
+
+        let count = this.state.count;
+
+        setTimeout(() => {
+            this.setState({count: count + num});
+        }, 1000);
+    };
 
     render() {
-        return (
-            <div className="container">
-                <Search  setSearchName={this.setSearchName}/>
-                <Main searchName={this.state.searchName}/>
 
+        const {count} = this.state;
+
+        return (
+            <div>
+                <p>Click {count} times</p>
+                <div>
+                    <select ref={select => this.select = select}>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                    </select>&nbsp;
+                    <button onClick={this.increment}>+</button>&nbsp;
+                    <button onClick={this.decrement}>-</button>&nbsp;
+                    <button onClick={this.incrementIfOdd}>increment if odd</button>&nbsp;
+                    <button onClick={this.incrementIfAsync}>increment if asyc</button>
+                </div>
             </div>
         );
     }

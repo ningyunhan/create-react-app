e55e927b9778f48102012cf3e1ce73eb31362631
ningyunhan <ningyunhan19940829@gmail.com>
2018-06-06 00:23:50 -0700

    redux devtools used

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
 import {counter} from './reducers';
 import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
 
 const store = createStore(
     counter,
-    applyMiddleware(thunk)
+    composeWithDevTools(applyMiddleware(thunk)),
+
 );
 
 export default store;
